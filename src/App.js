c11cb9718c49cb3d156acfa1d439abd699ef740e
hat-ad <dasashis672@gmail.com>
2021-06-07 13:03:24 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./Assets/css/bootstrap.min.css";
+import "./Assets/css/flex-slider.css";
+import "./Assets/css/font-awesome.css";
+import "./Assets/css/lightbox.css";
+import "./Assets/css/owl-carousel.css";
+import "./Assets/css/templatemo-klassy-cafe";
+
+import { Home } from "./Pages/index";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ src/Pages/index.js
@@ -0,0 +1,4 @@
+import Home from "./HomePage";
+import Admin from "./AdminPage";
+
+export { Home, Admin };
